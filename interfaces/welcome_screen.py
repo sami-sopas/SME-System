3a81aca78007a22cd020620276d61e62b1d265ba
david-vargas98 <edgar981125@live.com.mx>
2025-05-04 22:18:13 -0600

    feat/refactor: single window
    A logic separation was made to avoid closing the main window. Thus, when the 'Iniciar' button is hit, the next screen Will be displayed in the same root window.

--- interfaces/welcome_screen.py
+++ interfaces/welcome_screen.py
@@ -22,34 +22,40 @@ def show_welcome():
     y = (root.winfo_screenheight() // 2) - (height // 2)
     root.geometry(f"{witdth}x{height}+{x}+{y}")
 
-    #Titulo y subtitulo de la ventana
-    ctk.CTkLabel(root, text="SME System", font=ctk.CTkFont(size=24, weight="bold"), text_color="mediumblue").pack(pady=10)
-    ctk.CTkLabel(root, text="Sistema inteligente de recomendación de computadoras", font=ctk.CTkFont(size=18)).pack(pady=5)
+    def load_welcome():
+        #Esto es para eliminar la ventana anterior
+        for widget in root.winfo_children():
+            widget.destroy()
 
-    #Imagen perrona de la ventana
-    image_path = os.path.join("assets", "yanpol.png")
-    if os.path.exists(image_path):
-        image = Image.open(image_path)
-        img = ctk.CTkImage(image, size=(200, 200))
-        ctk.CTkLabel(root, image=img, text="").pack(pady=10)
+        #Titulo y subtitulo de la ventana
+        ctk.CTkLabel(root, text="SME System", font=ctk.CTkFont(size=24, weight="bold"), text_color="mediumblue").pack(pady=10)
+        ctk.CTkLabel(root, text="Sistema inteligente de recomendación de computadoras", font=ctk.CTkFont(size=18)).pack (pady=5)
 
-    #Botón de inicio
-        start_btn = ctk.CTkButton(
-        master= root,
-        text="Iniciar",
-        font=ctk.CTkFont(size=14, weight="bold"),
-        fg_color="transparent",
-        bg_color="transparent",
-        border_color="mediumblue",
-        hover_color="mediumblue",
-        border_width=2,
-        text_color="white",
-        corner_radius=10,
-        width=200,
-        image=ctk.CTkImage(light_image=image),
-        command=lambda: [root.destroy(), show_step_one()]
-    ).pack(pady=20)
+        #Imagen perrona de la ventana
+        image_path = os.path.join("assets", "yanpol.png")
+        if os.path.exists(image_path):
+            image = Image.open(image_path)
+            img = ctk.CTkImage(image, size=(200, 200))
+            ctk.CTkLabel(root, image=img, text="").pack(pady=10)
 
+        #Botón de inicio
+            start_btn = ctk.CTkButton(
+            master= root,
+            text="Iniciar",
+            font=ctk.CTkFont(size=14, weight="bold"),
+            fg_color="transparent",
+            bg_color="transparent",
+            border_color="mediumblue",
+            hover_color="mediumblue",
+            border_width=2,
+            text_color="white",
+            corner_radius=10,
+            width=200,
+            image=ctk.CTkImage(light_image=image),
+            command=lambda: show_step_one(root)
+        ).pack(pady=20)
+
+    load_welcome()
     root.mainloop()
 
 if __name__ == "__main__":
