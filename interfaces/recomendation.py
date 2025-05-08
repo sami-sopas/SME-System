3362dac1a23b1a8e9eccaffda970fd55ace6aaac
david-vargas98 <edgar981125@live.com.mx>
2025-05-08 16:43:16 -0600

    feat: functions, scrollable frame, console messages, variable assignment and modified conditions
    two functions were created to show the recomendations throught the screen; the first one to show less than five of them, and the other one to show more than five of them, was achieved by dding a scrollable frame, some messages were added to debug (we Will get rid of them at the end of development), the 'portabilidad' value assigment was corrected, some comments were added and finally, the results verification was refactored to use the functions previosly created and the conditions were modified for this.

--- interfaces/recomendation.py
+++ interfaces/recomendation.py
@@ -3,6 +3,27 @@ from PIL import Image
 import os
 from pyswip import Prolog
 
+#Función para crear la vista final de recomendaciones
+def make_window(root, resultados):
+    ctk.CTkLabel(root, text="Recomendación encontrada:", font=("Arial", 16)).pack(pady=10)
+    for resultado in resultados:
+        recomendacion = resultado["Recomendacion"]
+        ctk.CTkLabel(root, text=recomendacion, font=("Arial", 14), wraplength=500, justify="center").pack(pady=10)
+        print("Recomendacion: ", recomendacion)
+
+#Función para crear la vista final de recomendaciones con scroll
+def make_window_with_scroll(root, resultados):
+    #Creación del contenedor scrollable
+    scrollable_frame = ctk.CTkScrollableFrame(root, width=600, height=300)
+    scrollable_frame.pack(padx=20, pady=10, fill="both", expand=True)
+
+    ctk.CTkLabel(scrollable_frame, text="Recomendaciones encontradas:", font=("Arial", 16)).pack(pady=10)
+    for resultado in resultados:
+        recomendacion = resultado["Recomendacion"]
+        ctk.CTkLabel(scrollable_frame, text=recomendacion, font=("Arial", 14), wraplength=500, justify="center").pack(pady=10)
+        print("Recomendacion: ", recomendacion)
+
+#Función para crear las recomendaciones finales
 def show_final_recommendation(root, datos):
 
     print("Datos a prolog")
@@ -21,13 +42,13 @@ def show_final_recommendation(root, datos):
     else:
         # Si no existe 'gpu' en los datos, asignar 'no' como valor por defecto
         gpu = datos.get("gpu", "no")
-    
+    print("Portatil:", datos.get("portatil"))
     # Convertir 'portatil' a 'sí' o 'no' según el valor booleano
-    portabilidad = "si" if datos.get("portatil") else ""
+    portabilidad = "si" if datos.get("portatil") else "no"
     
     presupuesto = datos["presupuesto"]
     sistema = datos["sistema"]
-    print(tipo_uso, gpu, portabilidad, presupuesto, sistema)
+    print(f"Tipo de uso: {tipo_uso}, GPU: {gpu}, Portabilidad: {portabilidad}, Presupuesto: {presupuesto}, Sistema: {sistema}")
     # Consultar la recomendación con los parámetros obtenidos
     resultados = list(prolog.query(
         f'recomendar("{tipo_uso}", "{gpu}", "{portabilidad}", "{presupuesto}", "{sistema}", Recomendacion)'
@@ -39,17 +60,18 @@ def show_final_recommendation(root, datos):
     # Obtener la recomendación o mostrar un mensaje por defecto
     # recomendacion = resultados[0]["Recomendacion"] if resultados else "No se encontró una recomendación adecuada."
 
-    # Mostrar en ventana
+    #Limpiamos la pantalla
     for widget in root.winfo_children():
         widget.destroy()
-
+    
+    #Titulo de la ventana
     ctk.CTkLabel(root, text="✅ Recomendación Final", font=("Arial", 18, "bold")).pack(pady=20)
-    if resultados:
-        ctk.CTkLabel(root, text="Recomendación encontrada:", font=("Arial", 16)).pack(pady=10)
-        for resultado in resultados:
-            recomendacion = resultado["Recomendacion"]
-            ctk.CTkLabel(root, text=recomendacion, font=("Arial", 14), wraplength=500, justify="center").pack(pady=10)
-            print("Recomendacion: ", recomendacion)
+    
+    #Verificamos si hay resultados y mostramos la recomendación en base a la cantidad de resultados
+    if resultados and 1 <= len(resultados) <= 5:
+        make_window(root, resultados)
+    elif resultados and len(resultados) > 5:
+        make_window_with_scroll(root, resultados)
     else:
         ctk.CTkLabel(root, text="No se encontró una recomendación adecuada.", font=("Arial", 16)).pack(pady=10)
 
